02ab4bcd39bdf10925b92d4d8e8292b5fe005ca1
Anand Krishnan <101948161+Obamium37@users.noreply.github.com>
2022-09-06 18:50:38 -0700

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -9,13 +9,13 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDMnWOgZFj5sdpUrfQ80EKE1xZN7wHniqc",
-  authDomain: "pain-hours-logger.firebaseapp.com",
-  projectId: "pain-hours-logger",
-  storageBucket: "pain-hours-logger.appspot.com",
-  messagingSenderId: "337595358914",
-  appId: "1:337595358914:web:f285873fafd2fb7cf5e55b",
-  measurementId: "G-ZDQPM3EZ7N"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
